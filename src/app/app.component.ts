b7e63c5133450bc278e56cbac2812f822a724ca2
rami-majdoub <rami.majdoub1@gmail.com>
2022-07-06 19:23:11 +0100

    add wallet avatar (blockies)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { ConnectWalletService } from 'src/app/services/connect-wallet.service';
+import { createIcon } from '@download/blockies';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,7 @@ export class AppComponent {
   title = 'carbon-marketplace';
   connected = false;
   address = "";
+  imgUrl = "";
   
   constructor(
     public contractService_: ConnectWalletService,
@@ -18,6 +20,8 @@ export class AppComponent {
   
   async connect(){
     this.address = await this.contractService_.connectAccount();
+    this.imgUrl = createIcon({ seed: this.address.toLowerCase() }).toDataURL();
+
     this.address = this.address.substring(0, 5) + "..." + this.address.substring(this.address.length - 4, this.address.length);
     this.connected = true;
   }
