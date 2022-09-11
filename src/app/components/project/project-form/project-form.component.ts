86728db79abdcec4fd5bd7fe0a80116aa42564fb
rami-majdoub <rami.majdoub1@gmail.com>
2022-09-11 15:12:26 +0100

    add web3.storage

--- src/app/components/project/project-form/project-form.component.ts
+++ src/app/components/project/project-form/project-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { CIDString, Web3Storage } from 'web3.storage'
 
 import { ContractService } from 'src/app/services/contract.service';
 
@@ -26,9 +27,21 @@ export class ProjectFormComponent implements OnInit {
 
   onSubmit(): void {
     if(!this.form.valid) return;
+    const fileContent = JSON.stringify(this.form.value);
 
-    console.log(this.form.value);
-    this.form.reset();
+    const API_TOKEN_WEB3_STORAGE = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDQwNmJhMmY3QTU4MzA1ODlkZDY0MTk5NkNmQ0JmQTM3YjY0NGNDZDkiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTM4OTE0NTE3OTEsIm5hbWUiOiJ0ZXN0In0.nZ3WIpmlORmfgpdq_9MqrYY_RA7XMv1Be7j6BYW19YI";
+    const client = new Web3Storage({ token: API_TOKEN_WEB3_STORAGE });
+    const file = new File([fileContent], 'project.json', { type: 'text/plain' });
+    client.put([file])
+    .then(
+      (cid) => {
+        this.contractService_.connect();
+        return cid;
+      }
+    )
+    .then(this.contractService_.contract.functions.addProject)
+    .then(tx => tx.wait())
+    .then(console.log)
   }
 
   ngOnInit(): void {
