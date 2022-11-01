4cad88e2d069f584d459fdbad3275b8f2a7bbe0c
rami-majdoub <rami.majdoub1@gmail.com>
2022-11-01 07:21:01 +0100

    add project list

--- src/app/components/profile/table-list/table-list.component.ts
+++ src/app/components/profile/table-list/table-list.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Project } from 'src/app/models/project';
+import { ProjectService } from 'src/app/services/project.service';
 
 @Component({
   selector: 'app-table-list',
   templateUrl: './table-list.component.html',
   styleUrls: ['./table-list.component.css']
 })
-export class TableListComponent implements OnInit {
+export class TableListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+    private service: ProjectService,
+  ) { }
+
+  displayedColumns: string[] = ['id', 'location', 'name'];
+  querySubscription: Subscription | undefined;  
+  projects: Project[] = [];
 
   ngOnInit() {
+    this.querySubscription = this.service.getAll().subscribe(
+      ({ data }: { data: any }) => {
+      console.log(data);
+      this.projects = data.projects;
+    });
+  }
+
+  ngOnDestroy(): void {
+      this.querySubscription?.unsubscribe();
   }
 
 }
