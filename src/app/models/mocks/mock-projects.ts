b19dadd4be0bcbd48382a946ddedec958c6b38f5
rami-majdoub <rami.majdoub1@gmail.com>
2022-11-02 01:18:48 +0100

    update project mock data

--- src/app/models/mocks/mock-projects.ts
+++ src/app/models/mocks/mock-projects.ts
@@ -5,21 +5,9 @@ export const PROJECTS: Project[] = [
     id: 1,
     name: "‘El Arriero ’ Afforestation on degraded grasslands under extensive grazing project",
     location: "Uruguay",
-    method: "Agriculture Forestry and Other Land Use",
-    credits: 20_000,
-  },
-  { 
-    id: 1,
-    name: "‘El Arriero ’ Afforestation on degraded grasslands under extensive grazing project",
-    location: "Uruguay",
-    method: "Agriculture Forestry and Other Land Use",
-    credits: 20_000,
-  },
-  { 
-    id: 1,
-    name: "‘El Arriero ’ Afforestation on degraded grasslands under extensive grazing project",
-    location: "Uruguay",
-    method: "Agriculture Forestry and Other Land Use",
-    credits: 20_000,
+    methods: "Agriculture Forestry and Other Land Use",
+    description: "",
+    owner: "",
+    report: ""
   },
 ];
